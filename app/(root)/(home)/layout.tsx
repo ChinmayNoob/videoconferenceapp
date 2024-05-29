cd705d0e589539fcec0dc306cb7fbc3b8a614996
ChinmayNoob <chinmaysk0408@gmail.com>
2024-05-29 18:44:59 +0530

    sidebar setup done

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+import SideBar from '@/components/SideBar'
 import { Sidebar } from 'lucide-react'
 import React, { ReactNode } from 'react'
 
@@ -7,7 +8,7 @@ function HomeLayout({children}:{children : ReactNode}) {
     <main className='relative'>
         <Navbar /> 
         <div className='flex'>
-            <Sidebar />
+            <SideBar />
             <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
                 <div className='w-full'>
                     {children}
